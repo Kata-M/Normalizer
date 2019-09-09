a688e608f8dbe73f23bd0b0956de0d9a7525a08c
Kata M <kata@wlan075191.mobiel.utwente.nl>
2019-09-09 23:14:31 +0200

    clean code

--- normalizer.java
+++ normalizer.java
@@ -3,17 +3,11 @@ import java.util.regex.*;
 
 class Normalizer
 { 
-    // Your program begins with a call to main(). 
-    // Prints "Hello, World" to the terminal window. 
+
     public static void main(String args[]){ 
-        
- 
-        System.out.println("\n"); 
 
         //String fileName = "F-train146.txt";
         String fileName = "test.txt";
-
-        // This will reference one line at a time
         String line = null;
         String allText = "";
         
@@ -27,13 +21,13 @@ class Normalizer
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             while((line = bufferedReader.readLine()) != null) {
-               // System.out.println(line);
-            	System.out.println(line);
-       
-            	   String str = RemoveDuplicates(line);
-                   StringBuilder sb = new StringBuilder(str);
-                   System.out.println(sb.reverse().toString());
-            	
+
+        	   System.out.println(line);
+   
+        	   String str = RemoveDuplicates(line);
+               StringBuilder sb = new StringBuilder(str);
+               System.out.println(sb.reverse().toString());
+        	
 
             	System.out.println("\n");
             	
@@ -52,8 +46,6 @@ class Normalizer
             System.out.println(
                 "Error reading file '" 
                 + fileName + "'");                  
-            // Or we could just do this: 
-            // ex.printStackTrace();
         }        
 
         //System.out.println(allText); 
@@ -62,19 +54,10 @@ class Normalizer
 
 
     private static String RemoveDuplicates(String str) {
-	   // String str = "aaabbbccc";
 	    String outputString = new StringBuilder(str).reverse().toString();
-		// outputString is now hiah
-		//outputString = outputString.replaceAll("(.)(?=.*\\1)", "");
-		//outputString = outputString.replaceAll("(o)(?=o+\\1)", "Ö");
-      
-        outputString = outputString.replaceAll("(?i)(a|e|i|o|u)(?i)(?=(a|e|i|o|u)+\\1)", "Ö");
-        
-		
-
-		
-		//(?i)a{3,}|(?i)e{3,}|(?i)i{3,}|(?i)o{3,}|(?i)u{3,}
-	    //String filtered = str.replace(/[^\w\s]|(.)\1/gi, ""); 
+		//outputString = outputString.replaceAll("(.)(?=.*\\1)", "");    
+        //remove dublicates, replace them with empty string
+        outputString = outputString.replaceAll("(?i)(a|e|i|o|u)(?i)(?=(a|e|i|o|u)+\\1)", "");
 	    return outputString; 
 	}
 } 
